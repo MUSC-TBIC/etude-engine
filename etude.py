ea4c7bd556578ddcd9582602377e8d7cc4f49cda
Paul M. Heider <heiderp@musc.edu>
2017-05-23 16:22:36 -0400

    most common metrics allowed; order defined by command line argument
    You can now choose any of the most common metrics reported in scoring
    and have them presented in an arbitrary order.  New functions were
    added to compute these values to make error handling easier to
    cordon off.

--- etude.py
+++ etude.py
@@ -10,6 +10,7 @@ import warnings
 import re
 import xml.etree.ElementTree as ET
 
+import numpy as np
 import pandas as pd
 
 
@@ -60,42 +61,106 @@ def extract_annotations( ingest_file ,
                                            end_attribute = 'end' ,
                                            default_score = 'FP' )
 
-def norm_summary( score_summary ):
+def accuracy( tp , fp , tn , fn ):
+    if( tp + fp + tn + fn > 0 ):
+        return ( tp + tn ) / float( tp + fp + tn + fn )
+    else:
+        return 0.0
+
+
+def precision( tp , fp ):
+    if( fp + tp > 0 ):
+        return tp / float( fp + tp )
+    else:
+        return 0.0
+
+
+def recall( tp , fn ):
+    if( fn + tp > 0 ):
+        return tp / float( fn + tp )
+    else:
+        return 0.0
+
+
+def specificity( tn , fn ):
+    if( tn + fn > 0 ):
+        return tn / float( tn + fn )
+    else:
+        return 0.0
+
+
+def f_score( p , r , beta = 1 ):
+    if( p + r > 0 ):
+        return ( 1 + beta**2 ) * ( ( p * r ) / ( p + r ) )
+    else:
+        return 0.0
+
+
+def norm_summary( score_summary , row_name , args ):
+    ## Source for definitions:
+    ## -- https://en.wikipedia.org/wiki/Precision_and_recall#Definition_.28classification_context.29
+    ##
     score_types = score_summary.keys()
     ## First, we want to make sure that all score types are represented
     ## in the summary series.
     if( 'TP' not in score_types ):
-        score_summary[ 'TP' ] = 0
+        score_summary[ 'TP' ] = 0.0
     if( 'FP' not in score_types ):
-        score_summary[ 'FP' ] = 0
+        score_summary[ 'FP' ] = 0.0
     if( 'TN' not in score_types ):
-        score_summary[ 'TN' ] = 0
+        score_summary[ 'TN' ] = 0.0
     if( 'FN' not in score_types ):
-        score_summary[ 'FN' ] = 0
-    return score_summary
+        score_summary[ 'FN' ] = 0.0
+    ## True Positive Rate (TPR), Sensitivity, Recall, Probability of Detection
+    if( 'Recall' in args.metrics_list or
+        'F1' in args.metrics_list ):
+        score_summary[ 'Recall' ] = recall( tp = score_summary[ 'TP' ] ,
+                                            fn = score_summary[ 'FN' ] )
+    if( 'Sensitivity' in args.metrics_list ):
+        score_summary[ 'Sensitivity' ] = recall( tp = score_summary[ 'TP' ] ,
+                                                 fn = score_summary[ 'FN' ] )
+    ## Positive Predictive Value (PPV), Precision
+    if( 'Precision' in args.metrics_list or
+        'F1' in args.metrics_list ):
+        score_summary[ 'Precision' ] = precision( tp = score_summary[ 'TP' ] ,
+                                                  fp = score_summary[ 'FP' ] )
+    ## True Negative Rate (TNR), Specificity (SPC) 
+    if( 'Specificity' in args.metrics_list ):
+        score_summary[ 'Specificity' ] = specificity( tn = score_summary[ 'TN' ] ,
+                                                      fn = score_summary[ 'FN' ] )
+    ## Accuracy
+    if( 'Accuracy' in args.metrics_list ):
+        score_summary[ 'Accuracy' ] = accuracy( tp = score_summary[ 'TP' ] ,
+                                                fp = score_summary[ 'FP' ] ,
+                                                tn = score_summary[ 'TN' ] ,
+                                                fn = score_summary[ 'FN' ] )
+    ##
+    if( 'F1' in args.metrics_list ):
+        score_summary[ 'F1' ] = f_score( p = score_summary[ 'Precision' ] ,
+                                         r = score_summary[ 'Recall' ] )
+    ##
+    metrics = [ row_name ]
+    for metric in args.metrics_list:
+        metrics.append( score_summary[ metric ] )
+    return metrics
 
 
 def print_score_summary( score_card , file_list , args ):
     ## TODO - refactor score printing to a separate function
     ## TODO - add scores grouped by type
-    metric_list = [ 'TP' , 'FP' , 'TN' , 'FN' ]
     print( '{}{}{}'.format( '\n#########' ,
                             args.delim ,
-                            args.delim.join( '{}'.format( m ) for m in metric_list ) ) )
+                            args.delim.join( '{}'.format( m ) for m in args.metrics_list ) ) )
     ##
-    score_summary = norm_summary( score_card[ 'Score' ].value_counts() )
-    metrics = [ 'aggregate' ]
-    for metric in metric_list:
-        metrics.append( score_summary[ metric ] )
+    metrics = norm_summary( score_card[ 'Score' ].value_counts() ,
+                            'aggregate' , args )
     print( args.delim.join( '{}'.format( m ) for m in metrics ) )
     ##
     if( args.verbose ):
         for filename in file_list:
             this_file = ( score_card[ 'File' ] == filename )
-            score_summary = norm_summary( score_card[ this_file ][ 'Score' ].value_counts() )
-            metrics = [ filename ]
-            for metric in metric_list:
-                metrics.append( score_summary[ metric ] )
+            metrics = norm_summary( score_card[ this_file ][ 'Score' ].value_counts() ,
+                                    filename , args )
             print( args.delim.join( '{}'.format( m ) for m in metrics ) )
 
 
@@ -159,8 +224,20 @@ unstructured data extraction.
                         help="Directory containing gold reference set")
     parser.add_argument("test_dir",
                         help="Directory containing reference set to score")
-    
-    parser.add_argument("-d", dest = 'delim' , nargs = '?' ,
+
+    parser.add_argument( "-m" , "--metrics" , nargs = '+' ,
+                         dest = 'metrics_list' ,
+                         default = [ 'TP' , 'FP' , 'TN' , 'FN' ] ,
+                         choices = [ 'TP' , 'FP' , 'TN' , 'FN' ,
+                                     'Precision' ,
+                                     'Recall' , 'Sensitivity' ,
+                                     'Specificity' ,
+                                     'Accuracy' ,
+                                     'F1' ] ,
+                         help = "List of metrics to return, in order" )
+
+    parser.add_argument("-d", nargs = '?' ,
+                        dest = 'delim' ,
                         default = '\t' ,
                         help="Delimiter used in all output streams" )
 
